072be727bbc874ff4512b65b3d95935492347346
Lucchini Gianmaria <gianmaria.lucchini@gmail.com>
2018-12-03 11:35:56 +0100

    Display markers on map Added on click modal on marker

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {SingleReportViewComponent} from '../single-report-view/single-report-view.component';
+import {APIService} from '../services/api.service';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+
+declare var L;
 
 @Component({
   selector: 'app-map',
@@ -7,15 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  lat = 44.674546;
-  lng = 8.1213145;
-  constructor() { }
+    latitude: number = 44.4032971;
+    longitude: number = 8.9701358;
+    map: any;
+    reports: any;
 
-  ngOnInit() {
+    constructor(private apiService: APIService, private modalService: NgbModal) {
+    }
 
-  }
 
+    ngOnInit() {
+
+        this.map = L.map('map').setView([this.latitude, this.longitude], 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.map);
+
+        const that = this;
+
+        this.apiService.getReports().subscribe((data: Array<object>) => {
+
+            data.forEach(function(report) {
+                const marker = L.marker([report.latitude, report.longitude]).addTo(that.map);
+                console.log(report);
+                marker.on('click', () => that.markerOnClick(report));
+            });
+        });
+    }
+
+
+    markerOnClick(report) {
+        const modalRef = this.modalService.open(SingleReportViewComponent, {size: 'lg'});
+        modalRef.componentInstance.report = report;
+    }
 }
+
+
+
+
+
 
 
 
