f995b586938e5c46e94ee44fa5da3ae2c27dbb6d
Lucchini Gianmaria <gianmaria.lucchini@gmail.com>
2018-12-10 18:51:11 +0100

    Unit Test: report list and map

--- src/app/map/map.component.spec.ts
+++ src/app/map/map.component.spec.ts
@@ -1,14 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, inject, TestBed} from '@angular/core/testing';
 
 import { MapComponent } from './map.component';
+import {AgmCoreModule} from '@agm/core';
+import config from '../../../config.secret';
+import {HttpClientModule} from '@angular/common/http';
+import {APIService} from '../services/api.service';
 
 describe('MapComponent', () => {
   let component: MapComponent;
   let fixture: ComponentFixture<MapComponent>;
 
-  /*beforeEach(async(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MapComponent ]
+      declarations: [ MapComponent ],
+      imports: [
+          AgmCoreModule.forRoot({
+              apiKey: config.googleMapsApiKey,
+          }),
+          HttpClientModule
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +31,14 @@ describe('MapComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });*/
+  });
+
+  it('should have the same number of markers as the DB',
+      inject([APIService], async (apiService: APIService) => {
+          apiService.getReports().subscribe(reports => {
+              const expectedCount = Object.keys(reports).length;
+              const count = component.reports.length;
+              expect(count).toEqual(expectedCount);
+          });
+      }));
 });
--- src/app/reports-list/reports-list.component.spec.ts
+++ src/app/reports-list/reports-list.component.spec.ts
@@ -1,15 +1,6 @@
 import {async, ComponentFixture, inject, TestBed} from '@angular/core/testing';
 
 import { ReportsListComponent } from './reports-list.component';
-import {AppComponent} from '../app.component';
-import {MapComponent} from '../map/map.component';
-import {ReportsBoardComponent} from '../reports-board/reports-board.component';
-import {AddReportComponent} from '../add-report/add-report.component';
-import {SingleReportViewComponent} from '../single-report-view/single-report-view.component';
-import {PopupComponent} from '../popup/popup.component';
-import {BrowserModule} from '@angular/platform-browser';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {FormsModule} from '@angular/forms';
 import {AgmCoreModule} from '@agm/core';
 import config from '../../../config.secret';
 import {HttpClientModule} from '@angular/common/http';
@@ -21,19 +12,8 @@ describe('ReportsListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-        declarations: [
-            AppComponent,
-            MapComponent,
-            ReportsListComponent,
-            ReportsBoardComponent,
-            AddReportComponent,
-            SingleReportViewComponent,
-            PopupComponent,
-        ],
+        declarations: [ ReportsListComponent ],
         imports: [
-            BrowserModule,
-            NgbModule,
-            FormsModule,
             AgmCoreModule.forRoot({
                 apiKey: config.googleMapsApiKey,
             }),
