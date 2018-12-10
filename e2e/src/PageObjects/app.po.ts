2f50bddcd28b89d99905b1ec129097ef63de4e5e
Andrea Fasci <andreafasci@gmail.com>
2018-12-10 14:51:28 +0100

    Edited e2e adding report test Added id to addReportButton Folder created for PageObjects

--- e2e/src/PageObjects/app.po.ts
+++ e2e/src/PageObjects/app.po.ts
@@ -2,12 +2,9 @@ import { browser, by, element } from 'protractor';
 import {InsertFormPo} from './insert-form.po';
 
 export class AppPagePo {
+
   async navigateTo() {
     return await browser.get('/');
-  }
-
-  async getTitleText() {
-    return await element(by.xpath('/html/body/app-root/app-reports-board/div/div[1]/app-reports-list/div/div[1]/h1')).getText();
   }
 
   async clickAddReport() {
